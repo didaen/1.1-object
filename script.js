5a4873178ca4be71f20aeee3aa212e7a5d2a7f06
Dida En <den.elfsuju@gmail.com>
2022-01-19 04:47:57 +0700

    Mengomentari bagian Object Literal agar fokus pada FUNCTION DECLARATION.

--- script.js
+++ script.js
@@ -1,29 +1,29 @@
 // Cara untuk membuat Object pada JavaScript
 // 1. Object Literal
-let mahasiswa1 = {
-    //property
-    nama: 'Sandhika',
-    energi: 10,
+// let mahasiswa1 = {
+//     //property
+//     nama: 'Sandhika',
+//     energi: 10,
 
-    //method
-    makan: function (porsi) {
-        this.energi = this.energi + porsi;
-        console.log(`Selamat makan, ${this.nama}! Energimu sekarang menjadi ${this.energi}.`);
-    }
-}
+//     //method
+//     makan: function (porsi) {
+//         this.energi = this.energi + porsi;
+//         console.log(`Selamat makan, ${this.nama}! Energimu sekarang menjadi ${this.energi}.`);
+//     }
+// }
 
-// Object literal namanya tidak boleh sama    
-let mahasiswa2 = {
-    //property
-    nama: 'Edvin',
-    energi: 30,
+// // Object literal namanya tidak boleh sama
+// let mahasiswa2 = {
+//     //property
+//     nama: 'Edvin',
+//     energi: 30,
 
-    //method
-    makan: function (porsi) {
-        this.energi = this.energi + porsi;
-        console.log(`Selamat makan, ${this.nama}Energimu sekarang menjadi ${this.energi}.`);
-    }
-}
+//     //method
+//     makan: function (porsi) {
+//         this.energi = this.energi + porsi;
+//         console.log(`Selamat makan, ${this.nama}Energimu sekarang menjadi ${this.energi}.`);
+//     }
+// }
 
 
 // Function Declaration
