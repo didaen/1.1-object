d9fcfc95c47163067436f4d23fdd1c70313cf488
Dida En <den.elfsuju@gmail.com>
2022-01-19 04:42:42 +0700

    Memperbaiki error mengenai '}'. ERROR IS FIXED.

--- script.js
+++ script.js
@@ -10,25 +10,40 @@ let mahasiswa1 = {
         this.energi = this.energi + porsi;
         console.log(`Selamat makan, ${this.nama}! Energimu sekarang menjadi ${this.energi}.`);
     }
+}
 
 // Object literal namanya tidak boleh sama    
 let mahasiswa2 = {
-        //property
-        nama: 'Edvin',
-        energi: 30,
+    //property
+    nama: 'Edvin',
+    energi: 30,
 
-        //method
-        makan: function (porsi) {
-            this.energi = this.energi + porsi;
-            console.log(`Selamat makan, ${this.nama}! Energimu sekarang menjadi ${this.energi}.`);
-        }
+    //method
+    makan: function (porsi) {
+        this.energi = this.energi + porsi;
+        console.log(`Selamat makan, ${this.nama}Energimu sekarang menjadi ${this.energi}.`);
     }
-
-
-
-
+}
 
 
 // Function Declaration
+// Keuntungannya kalian tidak
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 // Constructor Function
 // Object.created
