bff3f89c6f459063da4b448702557b2e4e0ce966
Dida En <den.elfsuju@gmail.com>
2022-01-20 12:30:37 +0700

    Untuk constructor Function sebenarnya hampir sama dengan Function Declaratin. Maka dari itu ini semua yang dari Function Declaration cuma dicopy ke Contructor Function.

--- script.js
+++ script.js
@@ -31,6 +31,37 @@
 // Nanti kalo mau bikin objectnya 2 atau 3 tinggan instansiasinya yang berulangkali
 
 // Ini kita membuat BluE Print nya
+// function Mahasiswa(nama, energi) {
+//     // Awalnya sama yaitu dengan membuat object KOSONGAN
+//     let mahasiswa = {};
+//     mahasiswa.nama = nama;
+//     mahasiswa.energi = energi;
+
+//     // Ini juga mirip tapi ini bentuknya jadi seperti class
+//     // Pake . (dot)
+//     mahasiswa.makan = function (porsi) {
+//         this.energi += porsi;
+//         console.log(`Selamat makan, ${this.nama}. Energimu sekarang menjadi ${this.energi}.`);
+//     }
+
+//     // Membuat method main yang akan mengurangi energi
+//     mahasiswa.main = function (jam) {
+//         this.energi -= jam;
+//         console.log(`Hallo ${this.nama}, selamat bermain! Energimu sekarang tinggal ${this.energi}.`);
+//     }
+
+//     return mahasiswa;
+// }
+
+// // INISIASI OBJECT
+// let dida = Mahasiswa("Dida", 10);
+// let edvin = Mahasiswa('Edvin', 50);
+
+
+
+
+
+// Constructor Function
 function Mahasiswa(nama, energi) {
     // Awalnya sama yaitu dengan membuat object KOSONGAN
     let mahasiswa = {};
@@ -53,9 +84,6 @@ function Mahasiswa(nama, energi) {
     return mahasiswa;
 }
 
-// INISIASI OBJECT
-let dida = Mahasiswa("Dida", 10);
-let edvin = Mahasiswa('Edvin', 50);
 
 
 
@@ -69,5 +97,8 @@ let edvin = Mahasiswa('Edvin', 50);
 
 
 
-// Constructor Function
+
+
+
+
 // Object.created
